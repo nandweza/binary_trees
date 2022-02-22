a7921065964cd93f1e3ac94ce79511666df3a9c1
nandweza <allannandweza@gmail.com>
2022-02-22 00:29:37 -0800

    added task 24

--- binary_trees.h
+++ binary_trees.h
@@ -59,5 +59,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+int btib_helper(const binary_tree_t *tree, int min, int max);
 
 #endif /* BINARY_TREES_H */
